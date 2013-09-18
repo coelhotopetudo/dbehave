0cc640f5be8dd6b353d1779ee0f495a7e3c92841
alissoncoelhodemorais <alisson.coelho.de.morais@gmail.com>
2013-09-18 09:37:40 -0300

    Implementação até a parte de logger com @Inject e @RunWith

--- inscricao/src/main/java/br/gov/serpro/inscricao/Turma.java
+++ inscricao/src/main/java/br/gov/serpro/inscricao/Turma.java
@@ -1,11 +1,27 @@
 package br.gov.serpro.inscricao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
 public class Turma {
 	
+	@Inject
+	private Logger logger;
+
+	private List<String> alunosMatriculados = new ArrayList<String>();
+
 	public void matricular(String aluno) {
+		alunosMatriculados.add(aluno);
+		
+		logger.info("Aluno matriculado com sucesso");
+
 	}
 
 	public boolean estaMatriculado(String aluno) {
-		return false;
+		return alunosMatriculados.contains(aluno);
 	}
 }
--- inscricao/src/test/java/br/gov/serpro/inscricao/TurmaTest.java
+++ inscricao/src/test/java/br/gov/serpro/inscricao/TurmaTest.java
@@ -1,11 +1,23 @@
 package br.gov.serpro.inscricao;
 
-import org.junit.Test;
+import javax.inject.Inject;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.gov.frameworkdemoiselle.junit.DemoiselleRunner;
+
+@RunWith(DemoiselleRunner.class)
 public class TurmaTest {
 	
+	@Inject
+	private Turma turma;
+
 	@Test
 	public void matricularAlunoComSucesso() {
+		turma.matricular("Santos Dumont");
+		Assert.assertTrue(turma.estaMatriculado("Santos Dumont"));
 	}
 
 	@Test
